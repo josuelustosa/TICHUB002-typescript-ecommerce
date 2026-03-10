5749b11fb7ec578b8b460aa8c56a5dac48277e0c
Josué Lustosa <josuelustosa.job@gmail.com>
2026-03-10 18:23:35 -0400

    feat(ts): criando uma Class do objeto Category em model e acessando uma propriedade

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,9 +1,13 @@
 import { Product } from "./src/model/product.model.js";
+import { Category } from "./src/model/category.model.js";
+
+const category = new Category("Futebol");
 
 const product = new Product(
   "Chuteira Society Penalty",
   149.9,
   "https:example.site/igm/IMAGE-01.jpeg",
+  category,
 );
 
-console.log(product);
+console.log("Categoria do Produto:", product.category.title);
--- typescript/src/model/product.model.ts
+++ typescript/src/model/product.model.ts
@@ -1,7 +1,10 @@
+import type { Category } from "./category.model.js";
+
 export class Product {
   constructor(
     public title: string,
     public price: number,
     public img: string,
+    public category: Category,
   ) {}
 }

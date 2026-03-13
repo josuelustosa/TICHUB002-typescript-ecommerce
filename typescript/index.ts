dd7c942998c656faf6bbf8945d61ef419a522842
Josué Lustosa <josuelustosa.job@gmail.com>
2026-03-13 16:44:15 -0400

    feat(cart): criando uma Class Cart em model com método de verificar e adicionar item no objeto.

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,5 +1,6 @@
 import { Product } from "./src/model/product.model.js";
 import { Category } from "./src/model/category.model.js";
+import { Cart } from "./src/model/cart.model.js";
 
 const category = new Category("Futebol");
 
@@ -8,7 +9,16 @@ const product = new Product(
   149.9,
   "https:example.site/img/IMAGE-01.jpeg",
   category,
-  0.2,
+  0.5,
 );
 
-console.log("Categoria do Produto:", product.priceWithDiscountApplied());
+const cart = new Cart();
+cart.addItem(product, 1);
+cart.addItem(product, 4);
+
+console.log("Categoria do Produto:", product.category.title + `\n`);
+console.log(
+  "Valor do Produto com desconto aplicado: R$",
+  product.priceWithDiscountApplied() + `\n`,
+);
+console.log("Itens no carrinho: \n", cart.listProduct);
